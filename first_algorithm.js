177a26e7faca6951c16478e9b9e1298bf9e0de1a
Christophe  ADOTEVI <81805739+Christado@users.noreply.github.com>
2022-01-08 12:23:57 +0100

    eslint modif1a

--- first_algorithm.js
+++ first_algorithm.js
@@ -14,19 +14,19 @@ let filterRecipes = recipes;
 const searchByTag = (tagName,type) => {
   const input = normalizeValues(tagName)
   if (type === 'ingredient') {
-    filterRecipes = filterRecipes.filter((recipe)=> searchByIngredientPredicate(recipe, input)) 
+    filterRecipes = filterRecipes.filter((recipe)=> searchByIngredientPredicate(recipe, input))
     display.addRecipeToMainContainer(filterRecipes)
     selectedIngredient.push(tagName)
   }
   if (type === 'ustensil') {
 
-    filterRecipes = filterRecipes.filter((recipe)=> searchByUstensilPredicate(recipe, input)) 
+    filterRecipes = filterRecipes.filter((recipe)=> searchByUstensilPredicate(recipe, input))
     display.addRecipeToMainContainer(filterRecipes)
     selectedUstensil.push(tagName)
   }
   if (type === 'appliance') {
 
-    filterRecipes = filterRecipes.filter((recipe)=> searchByAppliancePredicate(recipe, input)) 
+    filterRecipes = filterRecipes.filter((recipe)=> searchByAppliancePredicate(recipe, input))
     display.addRecipeToMainContainer(filterRecipes)
     selectedAppli.push(tagName)
   }
@@ -38,20 +38,20 @@ const removeTag = (tagName,type) => {
   console.log('suppresion du tag')
   console.log(tagName)
   if (type === 'ingredient' ) {
-   selectedIngredient = selectedIngredient.filter(ingredient => ingredient!= tagName) 
+   selectedIngredient = selectedIngredient.filter(ingredient => ingredient!= tagName)
   }
 
   if (type === 'ustensil' ) {
-    selectedUstensil = selectedUstensil.filter(ustensil => ustensil!= tagName) 
+    selectedUstensil = selectedUstensil.filter(ustensil => ustensil!= tagName)
    }
 
    if (type === 'appliance' ) {
-    selectedAppli = selectedAppli.filter(appliance => appliance!= tagName) 
+    selectedAppli = selectedAppli.filter(appliance => appliance!= tagName)
    }
 
 filterRecipes = procesSearch(textInput)
 //console.table(filterRecipes)
-searchByAllIngredient()  
+searchByAllIngredient()
 searchByAllUstensil()
 searchByAllAppliance()
 display.addRecipeToMainContainer(filterRecipes)
@@ -62,8 +62,8 @@ const searchByAllIngredient = () => {
  console.log(selectedIngredient)
   selectedIngredient.forEach(ingredient => {
     const input = normalizeValues(ingredient)
-    filterRecipes = filterRecipes.filter((recipe)=> searchByIngredientPredicate(recipe, input)) 
-    
+    filterRecipes = filterRecipes.filter((recipe)=> searchByIngredientPredicate(recipe, input))
+
 
   })
 }
@@ -72,9 +72,9 @@ const searchByAllUstensil = () => {
   console.log(selectedUstensil)
    selectedUstensil.forEach(ustensil => {
      const input = normalizeValues(ustensil)
-     filterRecipes = filterRecipes.filter((recipe)=> searchByUstensilPredicate(recipe, input)) 
-     
- 
+     filterRecipes = filterRecipes.filter((recipe)=> searchByUstensilPredicate(recipe, input))
+
+
    })
  }
 
@@ -82,9 +82,9 @@ const searchByAllUstensil = () => {
   console.log(selectedAppli)
    selectedAppli.forEach(appliance => {
      const input = normalizeValues(appliance)
-     filterRecipes = filterRecipes.filter((recipe)=> searchByAppliancePredicate(recipe, input)) 
-     
- 
+     filterRecipes = filterRecipes.filter((recipe)=> searchByAppliancePredicate(recipe, input))
+
+
    })
  }
 
@@ -115,12 +115,12 @@ const searchByNamePredicate = (recipe,normalizedInput) => {
     const normalizedName = normalizeValues(recipe.name)
     return normalizedName.includes(normalizedInput)
   }
-  
+
   const searchByDescriptionPredicate = (recipe,normalizedInput) => {
     const normalizedDescription = normalizeValues(recipe.description)
     return normalizedDescription.includes(normalizedInput)
   }
-  
+
   const searchByIngredientPredicate = (recipe,normalizedInput) => {
     /*const ingredientNames = recipe.ingredients.map(item => item.ingredient)
     return ingredientNames.some(name => normalizeValues(name).includes(normalizedInput))*/
@@ -131,7 +131,7 @@ const searchByNamePredicate = (recipe,normalizedInput) => {
       if (normalizeValues(ingredientName).includes(normalizedInput)) {
         existe = true
       }
-      
+
     }
     return existe
   }
@@ -139,32 +139,34 @@ const searchByNamePredicate = (recipe,normalizedInput) => {
   const searchByUstensilPredicate = (recipe,normalizedInput) => {
     const ustensilNames = recipe.ustensils
     return ustensilNames.some(name => normalizeValues(name).includes(normalizedInput))
-    
+
   }
 
   const searchByAppliancePredicate = (recipe,normalizedInput) => {
     const applianceName = recipe.appliance
     return normalizeValues(applianceName).includes(normalizedInput)
-    
+
   }
 
   const procesSearch = (inputNorm) => {
     if (! inputNorm)
     return recipes
     /*return recipes.filter((recipe)=>{
-     
+
       return searchByNamePredicate(recipe,inputNorm) || searchByDescriptionPredicate(recipe,inputNorm) || searchByIngredientPredicate(recipe,inputNorm);
      })*/
       let result = []
      for (const recipe of recipes) {
 
-      if (searchByNamePredicate(recipe, inputNorm ) || searchByDescriptionPredicate(recipe, inputNorm) || searchByIngredientPredicate(recipe, inputNorm)) {
+      if (searchByNamePredicate(recipe, inputNorm ) 
+      || searchByDescriptionPredicate(recipe, inputNorm) 
+      || searchByIngredientPredicate(recipe, inputNorm)) {
         result.push(recipe)
       }
-       
+
      }
      return result
-    
+
     }
   const searchAlgo = (articles, input) => {
     const inputValueNorm = normalizeValues(input)
@@ -174,10 +176,9 @@ const searchByNamePredicate = (recipe,normalizedInput) => {
     //refrshDropDown(filterRecipes);
   };
 
-  
-  
-  
+
+
+
   export {
     searchAlgo,
   };
-  
